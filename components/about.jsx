539af50bfc0d8f2e96b62d6f9677ebbc7fbf9c7a
Arphie <arphie@Arphies-Mac-mini.local>
2026-02-26 18:25:03 +0800

    initial design

---
+++ components/about.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const About = (props) => {
+  return (
+    <div data-section id="about" className="mb-16 group">
+      <div className="text-surface-600">
+        {props.data.about.map(function (paragraph, index) {
+          return (
+            <div key={index} className="mb-6">
+              {paragraph}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default About;
